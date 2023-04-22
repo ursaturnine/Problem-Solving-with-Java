a7b015cc1e2d859717e36d8358f1813e4e3de085
Tyrah <tyrah96@gmail.com>
2023-04-22 16:06:10 -0700

    Reviewing nested loops with Java20

---
+++ Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator {
+    
+    
+    public void Calculate(){
+    for(int i = 1; i < 11; i++){
+        for(int j = 1; j <11; j++){
+            System.out.printf("%d X %d = %d\n", i, j, i*j);
+        };
+    };
+    }
+}
---
+++ Main.java
@@ -0,0 +1,8 @@
+
+class Main{
+    public static void main(String[] args){
+        Calculator calc = new Calculator();
+        calc.Calculate();
+
+    }
+}
